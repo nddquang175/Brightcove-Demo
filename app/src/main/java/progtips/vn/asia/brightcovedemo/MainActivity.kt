9fc596d5b2f806d8118d1b9a56f4f27eeedf617b
nddquang <quang.ngo@2359media.com>
2020-09-10 10:32:50 +0700

    First commit

---
+++ app/src/main/java/progtips/vn/asia/brightcovedemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package progtips.vn.asia.brightcovedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
